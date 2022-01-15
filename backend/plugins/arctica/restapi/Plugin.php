22661554b84f0f106b519e99d860c8486d171ad0
konellemhs <romaartemov1999@gmail.com>
2022-01-15 21:41:25 +0300

    правки, загрузка файлов

--- backend/plugins/arctica/restapi/Plugin.php
+++ backend/plugins/arctica/restapi/Plugin.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Request;
 use October\Rain\Exception\ValidationException;
 use System\Classes\PluginBase;
+use System\Classes\MediaLibrary;
+use Illuminate\Support\Facades\Event;
 
 /**
  * RestApi Plugin Information File
@@ -51,6 +53,29 @@ class Plugin extends PluginBase
                     'message' => $exception->getMessage(),
                 ], 400);
             }
+        });
+
+        Event::listen('media.file.upload', function($widget, $filePath, $uploadedFile) {
+            $originalName = $uploadedFile->getClientOriginalName();
+            $cyr = [
+                'а','б','в','г','д','е','ё','ж','з','и','й','к','л','м','н','о','п',
+                'р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю','я',
+                'А','Б','В','Г','Д','Е','Ё','Ж','З','И','Й','К','Л','М','Н','О','П',
+                'Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я'
+            ];
+            $lat = [
+                'a','b','v','g','d','e','io','zh','z','i','y','k','l','m','n','o','p',
+                'r','s','t','u','f','h','ts','ch','sh','sht','a','i','y','e','yu','ya',
+                'A','B','V','G','D','E','Io','Zh','Z','I','Y','K','L','M','N','O','P',
+                'R','S','T','U','F','H','Ts','Ch','Sh','Sht','A','I','Y','e','Yu','Ya'
+            ];
+            $cyr2lat = str_replace($cyr, $lat, $originalName);
+            $sanitizedFileName = str_replace(' ', '-', $cyr2lat);
+            $filePathChunks = explode(DIRECTORY_SEPARATOR, $filePath);
+            $filePathChunks[ (count($filePathChunks) - 1) ] = $sanitizedFileName;
+            $newPath = implode(DIRECTORY_SEPARATOR, $filePathChunks);
+            $isRename = true;
+            MediaLibrary::instance()->moveFile($filePath, $newPath, $isRename);
         });
     }
 
